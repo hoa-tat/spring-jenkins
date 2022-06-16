a4a41c972df63e90e74d509cf150127a9fbfcd65
oliver dat <you@example.com>
2022-06-16 14:40:52 +0700

    My first jenkins project

---
+++ src/test/java/com/hoatat/spring/jenkins/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,19 @@
+package com.hoatat.spring.jenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case executing...");
+		assertEquals(true,true);
+	}
+
+}
